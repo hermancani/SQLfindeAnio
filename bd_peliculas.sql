c6791d6f8d8b3ac1ae5162a14f0a15b8ef54cf22
Herman Caniulaf <31552287+hermancani@users.noreply.github.com>
2018-06-05 15:26:32 -0400

    BD poblada error en pelicula al aser insert into

--- bd_peliculas.sql
+++ bd_peliculas.sql
@@ -33,3 +33,21 @@ CREATE TABLE pelicula(-- DROP TABLE pelicula
 	pais VARCHAR(30)
 );-- SELECT * FROM pelicula;
 go
+SELECT * FROM generoPelicula
+SELECT * FROM actor
+SELECT * FROM director
+SELECT * FROM idioma
+SELECT * FROM pelicula
+
+INSERT INTO generoPelicula VALUES('Accion');
+INSERT INTO generoPelicula VALUES('Ficcion');
+INSERT INTO actor VALUES('La Roca');
+INSERT INTO actor VALUES('Brat pitt');
+INSERT INTO director VALUES('carlos barct');
+INSERT INTO director VALUES('Miguel boce');
+INSERT INTO idioma VALUES('ingles');
+INSERT INTO idioma VALUES('español');
+INSERT INTO pelicula VALUES('Rapidos y Furiosos','1','1','1','1','1',2017,'USA');
+INSERT INTO pelicula VALUES('Troya','2','2','2','2','2',2004,'Roma');
+
+DROP DATABASE bd_findeanio
