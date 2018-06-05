4b5d4bd1d09266b081d14486aee8c12768c8f23c
josedonoso <32175545+josedonoso@users.noreply.github.com>
2018-06-05 17:57:14 -0400

    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    SQL SERVER CON ACTUALIZACIONES DE TRABAJO
    
    
    XD
    XD
    XD
    XD

--- bd_peliculas.sql
+++ bd_peliculas.sql
@@ -41,13 +41,96 @@ SELECT * FROM pelicula
 
 INSERT INTO generoPelicula VALUES('Accion');
 INSERT INTO generoPelicula VALUES('Ficcion');
+INSERT INTO generoPelicula VALUES('Comedia');--g
+INSERT INTO generoPelicula VALUES('Cine');-- g
+INSERT INTO actor VALUES('Actor 1');-- a
 INSERT INTO actor VALUES('La Roca');
 INSERT INTO actor VALUES('Brat pitt');
 INSERT INTO director VALUES('carlos barct');
 INSERT INTO director VALUES('Miguel boce');
 INSERT INTO idioma VALUES('ingles');
 INSERT INTO idioma VALUES('español');
-INSERT INTO pelicula VALUES('Rapidos y Furiosos','1','1','1','1','1',2017,'USA');
-INSERT INTO pelicula VALUES('Troya','2','2','2','2','2',2004,'Roma');
+INSERT INTO pelicula VALUES('Pelicula','4','3','1','1','2018','Chle');-- nueva
+INSERT INTO pelicula VALUES('Pelicula 3','4','3','1','1','2018','Chle');-- nueva
+INSERT INTO pelicula VALUES('Pelicula 2','2','2','2','2','2018','Chle');-- nueva
+INSERT INTO pelicula VALUES('Rapidos y Furiosos','1','1','1','1',2017,'USA');
+INSERT INTO pelicula VALUES('Troya','2','2','2','2','2004','Roma');
 
-DROP DATABASE bd_findeanio
+-- DROP DATABASE bd_findeanio
+GO
+
+/* CONSULTA 1 --> PELICULAS*/
+
+SELECT
+	p.id,
+	p.nomPelicula AS 'Nombre de Pelicula',
+	g.nomGenero AS 'Genero',
+	a.nomActor AS 'Actor',
+	i.nomIdioma AS 'Idioma',
+	d.nomDirector AS 'Director',
+	p.anio AS 'Año',
+	p.pais AS 'Pais'
+FROM
+	pelicula p
+	INNER JOIN generoPelicula g
+	ON p.fk_genero = g.id
+	INNER JOIN actor a
+	ON p.fk_actor = a.id
+	INNER JOIN idioma i
+	ON p.fk_idioma = i.id
+	INNER JOIN director d
+	ON p.fk_director = d.id;
+
+	/* CONSULTA 2 --> GENERO Y SUS PELICULAS*/
+
+	SELECT
+		generoPelicula.nomGenero AS 'Genero',
+		pelicula.nomPelicula AS 'Pelicula'
+	FROM
+		pelicula pelicula
+		inner JOIN  generoPelicula
+		ON generoPelicula.id = pelicula.fk_genero
+		ORDER BY generoPelicula.nomGenero ASC;
+
+		-------------------------------------------
+		/* CONSULTA 3 --> IDIOMA Y SU PELICULA*/
+	SELECT
+		pelicula.nomPelicula AS 'Pelicula',
+		idioma.nomIdioma AS 'Idioma'
+	FROM
+		pelicula pelicula
+		inner JOIN  idioma
+		ON idioma.id = pelicula.fk_idioma
+		ORDER BY idioma.nomIdioma ASC;
+		-------------------------------------------
+
+		/* CONSULTA 4 --> PELICULA CON SU ACTOR Y DIRECTOR */
+
+	SELECT 
+		pelicula.nomPelicula AS 'Pelicula',
+		actor.nomActor AS 'Actor',
+		director.nomDirector AS 'Director'
+	FROM
+		pelicula,actor,director
+	WHERE
+		actor.id = pelicula.fk_genero AND
+		director.id=pelicula.fk_actor order by nomPelicula;
+
+		/* CONSULTA 5 -->  pelicula y su actor*/
+
+	SELECT 
+		p.nomPelicula AS 'Pelicula',
+		actor.nomActor AS 'Actor'
+	FROM
+		pelicula p
+		INNER JOIN actor
+		ON actor.id = p.fk_actor
+		ORDER BY actor.nomActor ASC;
+	
+		
+
+		
+		/*SELECT pelicula.nomPelicula as 'pelicula', COUNT(pelicula.fk_genero) AS 'genero' FROM pelicula
+		INNER JOIN generoPelicula ON generoPelicula.id = pelicula.fk_genero
+		GROUP BY pelicula.nomPelicula*/
+		
